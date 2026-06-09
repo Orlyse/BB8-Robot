5286b3bb007f8b1637583fde2d739b26f3347f10
Zhaohan Sun <zhaohansun2026@u.northwestern.edu>
2026-06-09 01:42:55 -0500

    The good stuff

--- CS346 Electrical/code files/receiver/main.c
+++ CS346 Electrical/code files/receiver/main.c
@@ -14,23 +14,21 @@
 #include "motor_pwm.h"
 #include "wireless_receive.h"
 #include "nrf_802154.h"
-#include"speaker.h"
 
 int main(void) {
     printf("Board started!\n");
 
     pwm_init();  
-    i2s_init();
     analog_input_pin_setup();
 
     // Configure 154 radio
     printf("About to init radio\n");
     nrf_802154_init();
     printf("Done with init\n");
-    nrf_802154_channel_set(16);
+    nrf_802154_channel_set(11);
     nrf_802154_auto_ack_set(false);
     nrf_802154_promiscuous_set(true);
-    uint8_t src_pan_id[] = {0xef, 0xef}; 
+    uint8_t src_pan_id[] = {0xcd, 0xab}; 
     nrf_802154_pan_id_set(src_pan_id);
     printf("Radio configured!\n");
 
@@ -44,16 +42,21 @@ int main(void) {
         printf("Could not enter receive mode\n");
     }
 
+    float direction = 0.0;
     // Loop forever
     while (1) {
-        play_sound();
-        /*
-        printf("Payload received ");
         for (int i=0; i<3; i++){
             printf("%02X ", payload[i]);
         }
-        */
         printf("\n");
-        nrf_delay_ms(2000);
+
+        move_car(direction, 0.25);
+
+        nrf_delay_ms(1000);
+
+        direction += 45.0;
+        if (direction == 360) {
+            direction = 0.0;
+        }
     }
 }
